d3e48493d98707f44dab16cd3c0564f21ad0b0fc
erico_andrei <erico_andrei@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-03-03 15:42:38 +0000

    Avoid locking out the ContentPanels by swalllowing this exception

--- Products/CMFContentPanels/skins/contentpanels/contentpanels_error.py
+++ Products/CMFContentPanels/skins/contentpanels/contentpanels_error.py
@@ -10,13 +10,16 @@
 
 # raise # uncomment this line for viewlet debug
 
-error=_['error']
-if error.type == 'Unauthorized':
-  return ""
-else:
-  err_log = context.error_log
+# This used to be a nice feature, but as it doesn't work, it's easier to show
+# a "friendly" error message instead of a Unauthorized error
+#error=_['error']
+#if error.type == 'Unauthorized':
+#  return ""
+#else:
+#  err_log = context.error_log
+#
+#  return """<p>An error ocurred.</p>
+#            <p>Error type: %s</p>
+#            <p>Error value: %s</p>""" % (error.type, error.value)
 
-  return """<p>An error ocurred.</p>
-            <p>Error type: %s</p>
-            <p>Error value: %s</p>""" % (error.type, error.value)
-
+return """<p>An error ocurred.</p>"""
