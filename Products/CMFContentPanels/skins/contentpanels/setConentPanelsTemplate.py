7e9d90b222c6c962b2dbd284b16fab92cd821e9c
Maik Derstappen <maik.derstappen@inqbus.de>
2011-11-22 20:35:10 +0100

    fix contentpanels_display_view to work in Plone 4, add an action to change the ContentPanals view if contentpanels_display_view is active on a folder, fix setConentPanelsTemplate.py to redirect to the context after setting he template

--- Products/CMFContentPanels/skins/contentpanels/setConentPanelsTemplate.py
+++ Products/CMFContentPanels/skins/contentpanels/setConentPanelsTemplate.py
@@ -1,3 +1,9 @@
+## Script (Python) "setContentPanelsTemplate"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
 ##parameters=contentpanels_template_id
 
 if not context.hasProperty('contentpanels_template_id'):
@@ -5,4 +11,4 @@ if not context.hasProperty('contentpanels_template_id'):
 else:
     context.manage_changeProperties({'contentpanels_template_id':contentpanels_template_id})
 
-return context.view()
+return context.REQUEST.RESPONSE.redirect(context.absolute_url())
